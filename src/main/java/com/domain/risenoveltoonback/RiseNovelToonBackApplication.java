b9364dc760264cb0511ebe8bfb3d90112e028daf
DESKTOP-ARKVPIG\nys <sjw07028@bnosoft.co.kr>
2026-08-03 10:11:42 +0900

    백엔드 코드 올리기

---
+++ src/main/java/com/domain/risenoveltoonback/RiseNovelToonBackApplication.java
@@ -0,0 +1,13 @@
+package com.domain.risenoveltoonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseNovelToonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiseNovelToonBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domain/risenoveltoonback/RiseNovelToonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.risenoveltoonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseNovelToonBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
